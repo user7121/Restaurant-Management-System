1c1b14311385f5c026dfdda239b2f64a4ef45346
Emir Şeker <158557458+user7121@users.noreply.github.com>
2026-03-25 16:12:19 +0300

    Create AdminDashboard.jsx

---
+++ frontend/src/pages/AdminDashboard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import CategoryManagement from "../components/Category/CategoryManagement";
+
+const AdminDashboard = () => {
+  return (
+    <div>
+      <h1>Admin Dashboard</h1>
+      <CategoryManagement />
+    </div>
+  );
+};
+
+export default AdminDashboard;
