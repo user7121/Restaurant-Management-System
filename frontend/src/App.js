45a4b648de509291add279352f4cfc66b362bbbc
Emir Şeker <158557458+user7121@users.noreply.github.com>
2026-03-25 16:14:10 +0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,17 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AdminDashboard from "./pages/AdminDashboard";
+// import Login from "./pages/Login"; (later)
 
 function App() {
-  return <AdminDashboard />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<AdminDashboard />} />
+        {/* <Route path="/login" element={<Login />} /> */}
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
